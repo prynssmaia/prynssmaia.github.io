d6e41d412e496b27fecf9e26d49cc23759b008b0
Prynss <prynss_lp@hotmail.com>
2022-05-19 23:13:35 -0300

    feat: :sparkles: create basic card structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./**/*.{html}"],
   theme: {
     colors: {
-      'body': '#17171F',
+      'card': '#0b0404f2',
       'selected-text': '#87021B',
       'theme': '#E10234',
       'white': '#ffffff',
@@ -13,8 +13,11 @@ module.exports = {
       'body': ["'Saira Condensed'", 'sans-serif']
     },
     backgroundImage: {
-     'hero': "url('/assets/bg-hero.jpg')"
-    }
+     'hero': "url('/assets/synth-landscape.png')"
+    },
+    boxShadow: {
+      'red': '0px 10px 24px rgba(225, 2, 52, 0.3)'
+    },
   },
   plugins: [],
 }
